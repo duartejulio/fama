605f96d67ce340591c566a13ddbdfaaaff61d5a6
duarte.julio@gmail.com <duarte.julio@gmail.com@925e8ead-fc8a-f197-f285-5a64ee607ce5>
2012-09-04 18:40:57 +0000

    liberada memoria do classificador alteração na forma de geração da mascara (a principio nao estava funcionando)

--- trunk/validador/validadorkdobras.cpp
+++ trunk/validador/validadorkdobras.cpp
@@ -13,29 +13,31 @@ ValidadorKDobras::~ValidadorKDobras()
 
 vector< vector< float > > ValidadorKDobras::executarExperimento( Treinador &treinador, Corpus &corpus, int atributoTreino, int atributoTeste )
 {
-    int qtd_sentencas = corpus.pegarQtdSentencas();
+    Classificador *classificador;
+    int i, j, qtd_sentencas = corpus.pegarQtdSentencas();
     vector< int> vetDobras;
     vector< vector< float > > resultado;
     vector< Corpus* > vetCorpus;
     vector< bool > vetMascara(qtd_sentencas);
 
-    for( int i = 0; i < numeroIteracoes; ++i )
-        vetDobras.assign(qtd_sentencas/numeroIteracoes, i);
+    for( i = 0; i < qtd_sentencas; ++i )
+        vetDobras.push_back(i%numeroIteracoes);
 
-    vetDobras.resize(qtd_sentencas, -1);
     random_shuffle( vetDobras.begin(), vetDobras.end() );
 
-    for( int i = 0; i < numeroIteracoes; ++i )
+    for( i = 0; i < numeroIteracoes; ++i )
     {
-        for( int j = 0; j < qtd_sentencas; ++j )
-            if( vetDobras[j] == i ) vetMascara[j] = true;
-            else vetMascara[j] = false;
+        for( j = 0; j < qtd_sentencas; ++j )
+            vetMascara[j] = (vetDobras[j] == i);
 
         vetCorpus = corpus.splitCorpus( vetMascara );
-        treinador.executarTreinamento( *vetCorpus[0], atributoTreino )->executarClassificacao( *vetCorpus[1], atributoTeste );
+        classificador = treinador.executarTreinamento( *vetCorpus[0], atributoTreino );
+        classificador->executarClassificacao( *vetCorpus[1], atributoTeste );
         resultado.push_back( avaliador->calcularDesempenho( *vetCorpus[1], atributoTreino, atributoTeste ) );
+
         delete vetCorpus[0];
         delete vetCorpus[1];
+        delete classificador;
     }
     return resultado;
 }
