133f162a2da5238633d98550c46efd220f784321
flplvr <flplvr@gmail.com>
2012-09-07 22:04:56 +0000

    Atualização da interface, criação de PopUp para inicialização de parâmetros de qualquer classe

---
+++ Interface/InterfaceFAMA/modeloparam.h
@@ -0,0 +1,36 @@
+#ifndef MODELOPARAM_H
+#define MODELOPARAM_H
+
+#include <QAbstractTableModel>
+#include <QFont>
+#include <QCheckBox>
+#include <QTableView>
+#include <vector>
+#include "param.h"
+
+class Param;
+
+class ModeloParam : public QAbstractTableModel
+{
+    Q_OBJECT
+public:
+    explicit ModeloParam(QObject *parent = 0);
+    int rowCount(const QModelIndex &parent = QModelIndex()) const ;
+    int columnCount(const QModelIndex &parent = QModelIndex()) const;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
+    bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole);
+    Qt::ItemFlags flags(const QModelIndex &index) const ;
+    void inserirDados( int row, int column, std::string v, const QModelIndex &parent = QModelIndex() );
+    void inserirDados(int row, int column, Param &ct, QWidget *x, std::string aux = "", const QModelIndex &parent = QModelIndex() );
+    bool isWidget(int row, int column);
+    bool removeRows(int row, int count, const QModelIndex &parent = QModelIndex());
+signals:
+    
+public slots:
+
+private:
+    std::vector< std::vector< std::pair<bool, std::string > > > Dados;
+    
+};
+
+#endif // MODELOPARAM_H
---
+++ Interface/InterfaceFAMA/param.h
@@ -0,0 +1,40 @@
+#ifndef PARAM_H
+#define PARAM_H
+
+#include <QDialog>
+#include <QAbstractItemModel>
+#include <QSpinBox>
+#include <vector>
+#include <sstream>
+#include "modeloparam.h"
+
+namespace Ui {
+class Param;
+}
+
+class Param : public QDialog
+{
+    Q_OBJECT
+    
+public:
+    explicit Param(QWidget *parent = 0);
+    ~Param();
+    void ajustarModelo( QAbstractItemModel &m );
+    int iniciarDialog();
+    std::string pegarString( int row );
+    QWidget *pegarWidget( int row );
+
+    std::vector< std::pair< std::string, QWidget* > > parametros;
+private:
+    Ui::Param *ui;
+    QTableView* pegarView();
+
+    friend class ModeloParam;
+
+public slots:
+    void accept();
+    void escolherAtributos( bool state );
+    void redimensionarAtributos( int tam );
+};
+
+#endif // PARAM_H
