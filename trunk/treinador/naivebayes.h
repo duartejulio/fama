158720186d219eed11cd3c3391c191462ffa1516
duarte.julio@gmail.com <duarte.julio@gmail.com@925e8ead-fc8a-f197-f285-5a64ee607ce5>
2012-09-18 17:59:32 +0000

    separados os arquivos de treinamento e classificação do NaiveBayes adicionado método de descricaoConhecimento no NaiveBayes

--- trunk/treinador/naivebayes.h
+++ trunk/treinador/naivebayes.h
@@ -1,22 +1,5 @@
 #include "../treinador/treinador.h"
-#include "../classificador/classificador.h"
-
-//prototipo do classificador
-class ClassificadorNB:public Classificador{
-    private:
-        vector <string> classes, atributos;
-        vector< vector < float > > medias, desvio;
-        vector < int > count;
-    public:
-        ClassificadorNB(vector<string> clas, vector<string> atr,
-                        vector< vector < float > > medi,
-                        vector< vector < float > > desv,
-                        vector<  int >);
-        bool executarClassificacao( Corpus &corpusProva, int atributo );
-        bool gravarConhecimento( string arquivo ){return true;}
-        bool carregarConhecimento( string arquivo ){return true;}
-        string descricaoConhecimento();
-};
+#include "../classificador/classificadornb.h"
 
 //prototipo do treinador
 class NaiveBayes:public Treinador{
