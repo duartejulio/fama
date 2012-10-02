c53cbe8013d137473a0aeea60a5261a86e1af5ec
duarte.julio <duarte.julio@gmail.com>
2012-10-02 17:48:06 +0000

    primeira implementação (ainda com problemas) do algoritmo ID3 no FAMA.

---
+++ testes/mainID3.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <cstdio>
+#include <cstdlib>
+
+#include "../id3/dtree.h"
+#include "../corpus/corpusmatriz.h"
+#include "../avaliador/avaliador_acuracia.h"
+
+using namespace std;
+
+int main()
+{
+    vector<string> atributos, classes, atributosTreino;
+    int nExemplos, c, nConjExemplos, e, c0, c1, iResposta, nTotalExemplos;
+
+    //carrega conjunto de dados
+    CorpusMatriz objCorpus(vector<string>(), ',', false, true);
+    objCorpus.carregarArquivo( "../inputs/data.data" );
+    iResposta = objCorpus.pegarPosAtributo("Purchase");
+
+    cout << "atributo alvo: " << iResposta << endl;
+    cout << (nTotalExemplos = objCorpus.pegarQtdTotalExemplos()) << " exemplos\n";
+
+    //indica classes alvo
+    classes.push_back("wontbuy");
+    classes.push_back("willbuy");
+    c0 = c1 = 0;
+
+    nConjExemplos = objCorpus.pegarQtdConjExemplos();
+    for (c=0; c<nConjExemplos; c++){
+        nExemplos = objCorpus.pegarQtdExemplos(c);
+        for (e=0; e < nExemplos; e++){
+            if (objCorpus.pegarIndice(classes[0]) == objCorpus.pegarValor(0, e, iResposta))
+                c0++;
+            else
+                c1++;
+        }
+    }
+
+    cout << c0 << " won't buy / " << c1 << " will buy / " <<endl;
+
+    cout << 100.*c0/nTotalExemplos << " / " << 100.*c1/nTotalExemplos << endl;
+
+    atributos = objCorpus.pegarAtributos();
+
+    for (unsigned int i=0; i < atributos.size(); i++) {
+        cout << "\n Atributo " << i << ":  " << atributos[i];
+        // (atributos[i]!="Purchase")
+            atributosTreino.push_back(atributos[i]);
+    }
+
+    DecisionTree objTree(atributosTreino, classes);
+
+    Classificador *objClass = objTree.executarTreinamento(objCorpus, iResposta);
+
+    objCorpus.criarAtributo("algoritmo", "O");
+    objClass->executarClassificacao(objCorpus, iResposta+1);
+
+    AvaliadorAcuracia objAvalAcur;
+    printf( "\nAcuracia: %.2f%%\n", 100 * objAvalAcur.calcularDesempenho( objCorpus,
+     iResposta, iResposta + 1)[0] );
+
+    //objCorpus.gravarArquivo( "../inputs/data.gravar" );
+
+    delete objClass;
+
+    return 0;
+}
