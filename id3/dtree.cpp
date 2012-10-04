03ea1fd452ee5d4187da7d522226ae8fc5a55142
duarte.julio <duarte.julio@gmail.com>
2012-10-04 16:22:41 +0000

    Implementação do algoritmo RandomForest, ainda não testado devido a bugs na classe ID3.

--- id3/dtree.cpp
+++ id3/dtree.cpp
@@ -1,5 +1,91 @@
 #include "dtree.h"
 #include <cmath>
+#include <sstream>
+#include <algorithm>
+
+ComiteClassificador::ComiteClassificador(vector<Classificador*> com){
+    comite = com;
+}
+
+bool ComiteClassificador::executarClassificacao( Corpus &corpus, int atributo){
+
+    register unsigned int quantidadeMembros = comite.size(), i, nExemplos, nConjExemplos, c, e;
+    int atributoVoto, maior, imaior;
+    map<int, int> mapaVoto;
+    map<int, int >::iterator it;
+    vector<int> iVotos;
+
+    for (i=0; i < quantidadeMembros; i++){
+        ostringstream nomeVoto;
+        nomeVoto << "voto" << i;
+        atributoVoto = corpus.criarAtributo(nomeVoto.str());
+        iVotos.push_back(atributoVoto);
+        comite[i]->executarClassificacao(corpus, atributoVoto);
+    }
+
+    nConjExemplos = corpus.pegarQtdConjExemplos();
+    for (c=0; c<nConjExemplos; c++){
+        nExemplos = corpus.pegarQtdExemplos(c);
+        for (e=0; e<nExemplos; e++){
+            mapaVoto.clear();
+            for (i=0; i<quantidadeMembros; i++)
+                mapaVoto[corpus.pegarValor(c, e, iVotos[i])]++;
+            maior = -1;
+            imaior = 0;
+            for (it = mapaVoto.begin(); it!=mapaVoto.end(); it++)
+                if (maior < it->second){
+                    maior = it->second;
+                    imaior = it->first;
+                }
+            corpus.ajustarValor(c, e, atributo, imaior);
+        }
+
+    }
+
+    return true;
+}
+
+RandomForest::RandomForest(DecisionTree *dt, vector <string> atr, unsigned int n, unsigned int nAtr){
+    arvoreBase = dt;
+    atributos = atr;
+    quantidadeMembros = n;
+    numeroAtributos = nAtr;
+}
+
+Classificador* RandomForest::executarTreinamento( Corpus &corpus, int atributo ){
+    vector <string> atributosBase(atributos);
+    vector<string>::iterator it;
+    string atributoAlvo;
+    register unsigned int i;
+    vector < Classificador*> comite;
+
+    atributoAlvo = corpus.pegarAtributo(atributo);
+
+    //remove atributoalvo
+    for (i=0; i < atributosBase.size();i++)
+        if (atributosBase[i]==atributoAlvo)
+            break;
+    atributosBase.erase(atributosBase.begin() + i);
+
+    for (i=0; i<quantidadeMembros; i++){
+        vector <string> atributosAtuais(atributosBase);
+        //shuffle
+        random_shuffle(atributosAtuais.begin(), atributosAtuais.end());
+        //cut
+        while (atributosAtuais.size() > numeroAtributos)
+            atributosAtuais.pop_back();
+        //readiciona atributo alvo
+        atributosAtuais.push_back(atributoAlvo);
+        cout << "\nAtributos Atuais:";
+        for (register unsigned int j=0; j<atributosAtuais.size(); j++)
+            cout << atributosAtuais[j] << " | ";
+        arvoreBase->ajustarAtributos(atributosAtuais);
+        comite.push_back(arvoreBase->executarTreinamento(corpus, atributo));
+        cout << "***" << endl;
+    }
+
+    return new ComiteClassificador(comite);
+}
 
 ClassificadorTree::ClassificadorTree(vector<string> clas, TreeNo &EndNoRaiz):Classificador(){
     //guarda parametros
@@ -8,7 +94,7 @@ ClassificadorTree::ClassificadorTree(vector<string> clas, TreeNo &EndNoRaiz):Cla
 }
 
 bool ClassificadorTree::executarClassificacao( Corpus &corpus, int atributo ){
-    int i, nAmostras, nConjAmostras, cont;
+    int i, nAmostras, nConjAmostras;
     //determina tamanho do conjunto
     nConjAmostras = corpus.pegarQtdConjExemplos();
 
