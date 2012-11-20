bd3f38d59ee4577f67c4b6adfca4d0e24963e5b5
diegoesteves@gmail.com <diegoesteves@gmail.com@925e8ead-fc8a-f197-f285-5a64ee607ce5>
2012-11-20 22:28:13 +0000

    Versão ajuste para BLS. Pendente de automatizar.

--- trunk/testes/mainSH.cpp
+++ trunk/testes/mainSH.cpp
@@ -9,21 +9,23 @@
 #include "../avaliador/avaliador_acuracia.h"
 #include "../treinador/naivebayes.h"
 #include "../validador/validadorkdobras.h"
+#include "../classificador/classificadorbls.h"
+
 
 using namespace std;
 
 int main()
 {
     vector<string> atributos, classes, atributosTreino;
-    int janela = 50, dobras=2;
+    int janela = 4, dobras=2;
      //indice do atributo a aprender
-    int iResposta, iSaida;
+    int iResposta, iSaida, iSaidaBLS;
 
     //carrega conjunto de dados
     CorpusMatriz objCorpus(vector<string>(), ',', false, true);
     ProcessadorSerieHistorica psh(janela, "valor");
 
-    objCorpus.carregarArquivo( "../inputs/ativos" );
+    objCorpus.carregarArquivo( "../inputs/ativos_petr4" );
     if (psh.processarCorpus(objCorpus))
         cout << "Sucesso\n";
 
@@ -38,11 +40,14 @@ int main()
     classes.push_back("+1");
 
     NaiveBayes objNB(atributosTreino, classes);
+
     AvaliadorAcuracia objAvalAcur;
     ValidadorKDobras objValidador(objAvalAcur, dobras);
 
     iResposta = objCorpus.pegarPosAtributo("y");
-    iSaida = objCorpus.criarAtributo("teste");
+    iSaida = objCorpus.criarAtributo("saida_nb");
+    iSaidaBLS = objCorpus.criarAtributo("saida_bls");
+
 /*
     cout << "*OK" << endl;
     vector< vector< float > > v = objValidador.executarExperimento(objNB, objCorpus, iResposta, iSaida);
@@ -56,13 +61,27 @@ int main()
 */
 
 
+
     Classificador *objClass = objNB.executarTreinamento(objCorpus, iResposta);
     objClass->executarClassificacao(objCorpus, iSaida);
     cout << objClass->descricaoConhecimento();
 
-    cout << "Qualidade: " << 100.*objAvalAcur.calcularDesempenho(objCorpus, iResposta, iSaida)[0] << "%" << endl ;
-    objCorpus.gravarArquivo( "../inputs/#teste.txt" );
+    cout << "Qualidade Naive Bayes: " << 100.*objAvalAcur.calcularDesempenho(objCorpus, iResposta, iSaida)[0] << "%" << endl ;
 
+    ClassificadorBLS *cbls;
+    cbls = new ClassificadorBLS(classes);
+    cbls->executarClassificacao(objCorpus, iSaidaBLS);
+
+
+
+    cout << "Qualidade BLS: " << 100.*objAvalAcur.calcularDesempenho(objCorpus, iResposta, iSaidaBLS)[0] << "%" << endl ;
+
+    objCorpus.gravarArquivo( "../inputs/#teste_nb.txt" );
 
     return 0;
 }
+
+
+
+
+
