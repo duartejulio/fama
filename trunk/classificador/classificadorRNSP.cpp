d9e4ead9678bcb5abd0811bad91e7dc72fbd4ad7
duarte.julio@gmail.com <duarte.julio@gmail.com@925e8ead-fc8a-f197-f285-5a64ee607ce5>
2013-09-02 18:57:16 +0000

    arquivos referentes a rede neural sem pesos implementada por Crivela e Angonese na cadeira de AM necessita ainda de validação

---
+++ trunk/classificador/classificadorRNSP.cpp
@@ -0,0 +1,240 @@
+/*
+ * File:   classificadorRNSP.cpp
+ * Autores: Anderson Crivela e Alberto Angonese
+ *
+ * Created on August 14, 2013, 12:04 AM
+ */
+
+#include "classificadorRNSP.h"
+//#include "../outros/barraprogresso.h"
+#include <math.h>
+#include <sstream>
+#include <iomanip>
+
+template <typename T> T StringToNumber (const string &Text) {
+     istringstream ss(Text);
+     T result;
+     return ss >> result ? result : 0;
+}
+
+template <typename T>  string NumberToString ( T Number ) {
+     ostringstream ss;
+     ss << Number;
+     return ss.str();
+}
+
+ClassificadorRNSP::ClassificadorRNSP(int classes) {
+    this->TAB = "\t";
+    this->classes = classes;
+    validacao.resize(classes);
+    matriz_confusao.resize(classes);
+    for (register int i = 0; i < classes; i++)
+        matriz_confusao[i].resize(classes);
+}
+
+ClassificadorRNSP::ClassificadorRNSP(int classes, int qtdeExemplos) {
+    this->TAB = "\t";
+    this->classes = classes;
+    validacao.resize(classes);
+    matriz_confusao.resize(classes);
+    for (register int i = 0; i < classes; i++)
+        matriz_confusao[i].resize(classes);
+
+    this->qtdeExemplos = qtdeExemplos;
+}
+
+void ClassificadorRNSP::limparMatrizConfusao(int classes) {
+    for (register int i = 0; i < classes; i++)
+        for (register int j = 0; j < classes; j++)
+            matriz_confusao[i][j] = 0;
+}
+
+ClassificadorRNSP::~ClassificadorRNSP() {
+}
+
+bool ClassificadorRNSP::executarClassificacao(Corpus &corpus, int atributo){
+
+    int maior, posicao;
+    int i, j, m, n, r, classe;
+    int acertos, total_ativados, duplicados;
+    ofstream arq_validacao,arq_matriz_confusao;
+    string binario;
+    this->qtdeExemplos = 10;
+
+    cout << "Realizando classificacao..." << endl;
+
+    //Abrindo arquivo para resumo da validacao
+    string nome_arquivo_validacao = "validacao.txt";
+    arq_validacao.open(nome_arquivo_validacao.c_str());
+    arq_validacao << "n_classes\tn_treinamento\tn_bits\tn_rams\trepticao\tbits_rams\ttamanho_ram\ttotal\tacertos\tduplicados\n";
+
+    string nome_arquivo_matriz_confusao = "matriz_confusao.txt";
+    arq_matriz_confusao.open(nome_arquivo_matriz_confusao.c_str());
+
+    int posicaoBinario = corpus.pegarPosAtributo("binario");
+    for (unsigned int rede = 0; rede < redes.size(); rede++) {
+
+        limparMatrizConfusao(redes[rede].classes);
+
+        //Efetuando validacao dos padroes:
+        acertos = total_ativados = 0;
+        for (i = 0; i < redes[rede].classes; i++){
+            for (j = 0; j < redes[rede].classes; j++){
+                validacao[j] = 0;
+            }
+            for (j = 0; j < this->qtdeExemplos; j++){
+            //for (c = 0; c < corpus.pegarQtdConjExemplos(); c++){
+                //for (e = 0; e < corpus.pegarQtdExemplos(c); e++){
+                binario = corpus((i*this->qtdeExemplos)+j,0,posicaoBinario);
+                classe = StringToNumber<int>(corpus((i*this->qtdeExemplos)+j,0,posicaoBinario+1));
+                maior = 0;
+                for (m = 0; m < redes[rede].classes; m++){ //para cada discriminador
+                    for (n = 0; n < redes[rede].rams; n++){ //para cada ram
+                        posicao = 0;
+                        for (r = 0; r < redes[rede].bits_enderecamento; r++){//para cada posicao da ram
+                            binario[redes[rede].distribuicao_bits[n][r]] == '1' ? posicao += pow(2, redes[rede].bits_enderecamento - r - 1) : 0;
+                        }
+                        validacao[m] += redes[rede].discriminadores[m][n][posicao];
+                    }
+                    validacao[m] > maior ? maior = validacao[m] : 0;
+                }
+                //verificando quais discriminadores foram mais ativados e atualizando matriz de confusão
+                for (m = 0; m < redes[rede].classes; m++){
+                    if (validacao[m] == maior){
+                        matriz_confusao[i][m]++;
+                        total_ativados++;
+                        if (i == m){
+                            acertos++;
+                        }
+                    }
+                }
+                //corpus((i*this->qtdeExemplos)+j,0,2,"0");
+            }
+        }
+
+        //Gravando matriz de confusão
+        arq_matriz_confusao << "Matriz: " << redes[rede].rams << " - Repeticao: " << redes[rede].repeticao << endl;
+        for (i = 0; i < redes[rede].classes; i++){
+            for (j = 0; j < redes[rede].classes; j++){
+                arq_matriz_confusao << matriz_confusao[i][j] << TAB;
+            }
+            arq_matriz_confusao << endl;
+        }
+        arq_matriz_confusao << endl;
+
+        //Gravando linha no arquivo de resumo da validacao:
+        arq_validacao << redes[rede].classes << TAB;
+        arq_validacao << redes[rede].totalBitsRam << TAB;
+        arq_validacao << redes[rede].rams << TAB;
+        arq_validacao << redes[rede].repeticao << TAB;
+        arq_validacao << redes[rede].bits_enderecamento << TAB;
+        arq_validacao << redes[rede].tamanhoRam << TAB;
+        arq_validacao << redes[rede].classes * this->qtdeExemplos << TAB;
+        arq_validacao << acertos << TAB;
+
+        duplicados = total_ativados - (redes[rede].classes * this->qtdeExemplos);
+        arq_validacao << duplicados << TAB;
+        arq_validacao << endl;
+
+        if ( (acertos - duplicados) > maiorAcerto ) {
+            this->maiorAcerto = acertos;
+            this->maiorMatriz = matriz_confusao;
+            this->rams = redes[rede].rams;
+            this->maiorRepeticao = redes[rede].repeticao;
+        }
+
+//        loadbar(rede+1, redes.size(), 50);
+
+    }
+    arq_validacao.close();
+    arq_matriz_confusao.close();
+
+    imprimirResultado();
+
+    cout << "Classificacao finalizada..." << endl;
+    return true;
+}
+
+void ClassificadorRNSP::imprimirResultado() {
+    string classes[] = {"Raiva","Despre","Nojo","Medo","Felici","Neutro","Triste","Surpr"};
+    cout << "-----------------------------------------------------------------------------\n";
+    cout << endl << "           Matriz de confusao da melhor configuracao:   " << this->rams << " - " << this->maiorRepeticao << endl;
+    cout << "-----------------------------------------------------------------------------\n";
+    cout << TAB;
+    for (int i = 0; i < this->classes; i++)
+        cout << classes[i] << TAB ;
+    cout << endl;
+    for (int i = 0; i < this->classes; i++) {
+        cout << classes[i] << TAB;
+        for (int j = 0; j < this->classes; j++) {
+            cout << this->maiorMatriz[i][j] << TAB;
+        }
+        cout << endl;
+    }
+    cout << "-----------------------------------------------------------------------------\n";
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.setf(ios::showpoint);
+    cout << "Percentual de acertos: " << setprecision(2) <<  (this->maiorAcerto/(this->qtdeExemplos*this->classes))*100 << endl;
+    cout << "-----------------------------------------------------------------------------\n";
+}
+
+bool ClassificadorRNSP::gravarConhecimento( string arquivo ){
+
+    cout << "Gravando conhecimento..." << endl;
+
+    ofstream out;
+    out.open(arquivo.c_str());
+    out << conhecimento;
+    out.close();
+
+    cout << "Conhecimento gravado..." << endl;
+
+    return true;
+}
+
+bool ClassificadorRNSP::carregarConhecimento( string arquivo ){
+    ifstream in;
+    stringstream buffer;
+    string linha;
+    RedeNeural rede;
+
+    cout << "Carregando conhecimento..." << endl;
+
+    in.open(arquivo.c_str());
+    while (getline(in,linha)) {
+        buffer << linha;
+        buffer >> rede.classes;
+        buffer >> rede.totalBitsRam;
+        buffer >> rede.rams;
+        buffer >> rede.bits_enderecamento;
+        buffer >> rede.tamanhoRam;
+        buffer >> rede.repeticao;
+
+        rede.distribuicao_bits.resize(rede.rams);
+        for (int i = 0; i < rede.rams; i++){
+            rede.distribuicao_bits[i].resize(rede.bits_enderecamento);
+            for (int j = 0; j < rede.bits_enderecamento; j++){
+                buffer >> rede.distribuicao_bits[i][j];
+            }
+        }
+
+        rede.discriminadores.resize(rede.classes);
+        for (int i = 0; i < rede.classes; i++){
+            rede.discriminadores[i].resize(rede.rams);
+            for (int j = 0; j < rede.rams; j++){
+                rede.discriminadores[i][j].resize(rede.tamanhoRam);
+                for (int m = 0; m < rede.tamanhoRam; m++){
+                    buffer >> rede.discriminadores[i][j][m];
+                }
+            }
+        }
+        redes.push_back(rede);
+    }
+    in.close();
+
+    cout << "Conhecimento carregado..." << endl;
+
+    return true;
+};
+
+
---
+++ trunk/treinador/treinadorRNSP.cpp
@@ -0,0 +1,164 @@
+/*
+ * File:   treinadorRNSP.cpp
+ * Autores: Anderson Crivela e Alberto Angonese
+ *
+ * Created on August 14, 2013, 12:04 AM
+ */
+#include "treinadorRNSP.h"
+#include <math.h>
+#include <ostream>
+#include <iostream>
+#include <sys/time.h>
+//#include "../outros/barraprogresso.h"
+
+template <typename T>  string NumberToString ( T Number ) {
+     ostringstream ss;
+     ss << Number;
+     return ss.str();
+}
+
+template <typename T> T StringToNumber (const string &Text) {
+     istringstream ss(Text);
+     T result;
+     return ss >> result ? result : 0;
+}
+
+TreinadorRNSP::TreinadorRNSP(int totalbits, int classes, int bits_enderecamento, int repeticoes) {
+    this->totalBitsRam = totalbits;
+    this->classes = classes;
+    this->bits_enderecamento = bits_enderecamento;
+    this->repeticoes = repeticoes;
+    this->rams = totalbits/bits_enderecamento;
+    this->tamanhoRam = pow(2,bits_enderecamento);
+
+    //Alocando memória para os vetores
+    distribuidos.resize(totalBitsRam);
+
+    distribuicao_bits.resize(this->rams);
+    for (register int i = 0; i < this->rams; ++i) {
+        distribuicao_bits[i].resize(this->bits_enderecamento);
+    }
+
+    discriminadores.resize(this->classes);
+    for (register int i = 0; i < this->classes; ++i) {
+        this->discriminadores[i].resize(this->rams);
+        for (register int j = 0; j < this->rams; ++j) {
+            this->discriminadores[i][j].resize(this->tamanhoRam);
+        }
+    }
+    //Fim alocacao
+}
+
+TreinadorRNSP::~TreinadorRNSP() {
+
+}
+
+/* Executa o treinamento de redes neurais sem peso */
+Classificador *TreinadorRNSP::executarTreinamento( Corpus &corpus, int atributo ) {
+
+    const char* SPACE = " ";
+
+    int repeticao, h, i, j, m, n, posicao;
+    ostringstream saida;
+
+    cout << "Iniciando o treinamento..." << endl;
+
+    for (repeticao = 0; repeticao < this->repeticoes; repeticao++){
+
+        distribuirBitsAleatoriamente();
+
+        for (h = 1; h <= this->rams; h++){
+
+            //Zerando a matriz dos discriminadores
+            limparDiscriminadores();
+
+            //Preenchendo as RAMS
+            int c,e,classe;
+            string binario;
+            for (c = 0; c < corpus.pegarQtdConjExemplos(); c++){
+                for (e = 0; e < corpus.pegarQtdExemplos(c); e++){
+                    //binario = corpus(c,e,atributo);
+                   //classe = StringToNumber<int>(corpus(c,e,atributo+1));
+                   binario = corpus(c,e,0);
+                   classe = StringToNumber<int>(corpus(c,e,atributo));
+
+                    //Faz o treinamento pada cada padrão da classe
+                    for (m = 0; m < h; m++){
+                        posicao = 0;
+                        for (n = 0; n < this->bits_enderecamento; n++) {
+                            binario[distribuicao_bits[m][n]] == '1' ? posicao += pow(2, this->bits_enderecamento - n - 1) : 0;
+                        }
+                        discriminadores[classe][m][posicao] += 1;
+                    }
+                }
+            }
+
+             //Gravando parâmetros gerais do treinamento
+             saida << this->classes << SPACE;
+             saida << this->totalBitsRam << SPACE;
+             saida << h << SPACE;
+             saida << this->bits_enderecamento << SPACE;
+             saida << this->tamanhoRam << SPACE;
+             saida << repeticao+1 << SPACE;
+
+             //Gravando a distribuição
+             for (i = 0; i < h; i++){
+                 for (j = 0; j < this->bits_enderecamento; j++){
+                     saida << distribuicao_bits[i][j] << SPACE;
+                 }
+             }
+             //Gravando as RAMS
+             for (i = 0; i < this->classes; i++){
+                 for (j = 0; j < h; j++){
+                     for (m = 0; m < this->tamanhoRam; m++){
+                         saida << discriminadores[i][j][m] << SPACE;
+                     }
+                 }
+             }
+             saida << endl;
+         }
+      //  loadbar(repeticao+1, this->repeticoes, 50);
+     }
+
+    ClassificadorRNSP *cl;
+    cl = new ClassificadorRNSP(this->classes);
+    cl->conhecimento = saida.str();
+
+    cout << endl << "Treinamento finalizado..." << endl;
+
+    return cl;
+}
+
+/* Faz a distribuicao aleatoria de bits conforme a quantidade de
+ * bits de endereçamento.  */
+void TreinadorRNSP::distribuirBitsAleatoriamente() {
+
+    int aux;
+
+    for (register int i = 0; i < this->totalBitsRam; i++){
+        distribuidos[i] = 0;
+    }
+
+    timeval t1;
+    gettimeofday(&t1, NULL);
+    srand(t1.tv_usec * t1.tv_sec);
+    for (register int i = 0; i < this->rams; i++){
+        for (register int j = 0; j < this->bits_enderecamento; j++){
+            aux = rand() % this->totalBitsRam;
+            this->distribuicao_bits[i][j] = aux;
+            this->distribuidos[aux] == 1 ? j--: distribuidos[aux] = 1;
+        }
+    }
+}
+
+/* Zera a todos os discriminadores  */
+void TreinadorRNSP::limparDiscriminadores() {
+    for (register int i = 0; i < this->classes; i++){
+        for (register int j = 0; j < this->rams; j++){
+            for (register int m = 0; m < this->tamanhoRam; m++){
+                this->discriminadores[i][j][m] = 0;
+            }
+        }
+    }
+}
+
