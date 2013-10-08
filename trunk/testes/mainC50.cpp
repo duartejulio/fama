206d4a91eb67965956e15096005294caa4d50516
duarte.julio@gmail.com <duarte.julio@gmail.com@925e8ead-fc8a-f197-f285-5a64ee607ce5>
2013-10-08 13:09:21 +0000

    arquivos de teste do c5.0

--- trunk/testes/mainC50.cpp
+++ trunk/testes/mainC50.cpp
@@ -5,11 +5,12 @@
 #include <sstream>
 #include <sys/unistd.h>
 #include <sys/time.h>
-#include <sys/resource.h>
+//#include <sys/resource.h>
 #include "../C50/c50.h"
 #include "../corpus/corpusmatrizc50.h"
 #include "../classificador/classificadorc50.h"
 #include "../treinador/treinadorC50.h"
+#include "../avaliador/avaliador_acuracia.h"
 
 #define SetFOpt(V)	V = strtod(OptArg, &EndPtr);\
 			if ( ! EndPtr || *EndPtr != '\00' ) break;\
@@ -22,19 +23,26 @@
 int main()
 {
     C50 objC50;
-    
-    CorpusC50 objCorpus(vector<string>(), ',', true, true);    
-    objCorpus.carregarArquivo(objC50, "MalwareDinamicos" );
+
+    CorpusC50 objCorpus(vector<string>(), ',', true, true);
+    objCorpus.carregarArquivo(objC50, "../inputs/MalwareDadosTotais" );
     //objC50.imprimir();
      TreinadorC50 objTreinador(objCorpus.pegarAtributos(),objCorpus.pegarClasses(),objCorpus.pegarAttValores(),objC50, 0.9);
-   
-    Classificador *objClass = objTreinador.executarTreinamento(objCorpus,0);
+
+    int at = objCorpus.pegarPosAtributo("Maligno");
+
+    Classificador *objClass = objTreinador.executarTreinamento(objCorpus,at);
     cout<<"construiu o classificador"<<endl;
-    //int x = objCorpus.criarAtributo("Classificacao");
-  
+    int x = objCorpus.criarAtributo("Classificacao");
+
     cout<<"criou o atributo"<<endl;
-    objClass->executarClassificacao(objCorpus,28);
+    objClass->executarClassificacao(objCorpus,x);
     cout<<"terminou a classificacao"<<endl;
-    objCorpus.gravarArquivo("C:\\Users\\Leticia Cremasco\\Documents\\IP\\fama\\inputs\\corpusGravado3.txt");
+    objCorpus.gravarArquivo("#corpusGravado3.txt");
+
+	AvaliadorAcuracia objAvalAc;
+	cout << "Acuracia:   " << 100*objAvalAc.calcularDesempenho(objCorpus, at, x)[0] << " %" <<endl << endl;
+
+
     return 0;
 }
