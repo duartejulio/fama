a50c026d28b97f81bd9a62412af0e8867b3d91bc
duarte.julio <duarte.julio@gmail.com>
2014-04-15 13:20:35 +0000

    novo avaliador criado para calcular uma matriz de confusão

---
+++ avaliador/avaliadormatrizconfusao.h
@@ -0,0 +1,17 @@
+#ifndef AVALIADOR_MATRIZ_CONFUSAO_H
+#define AVALIADOR_MATRIZ_CONFUSAO_H
+
+#include "avaliador.h"
+
+class AvaliadorMatrizConfusao : public Avaliador
+{
+        vector<string> classes;
+    public:
+        AvaliadorMatrizConfusao(vector<string>);
+        virtual ~AvaliadorMatrizConfusao();
+        virtual vector<float> calcularDesempenho( Corpus &corpus, int atributo_padrao, int atributo_teste );
+        virtual vector<float> calcularDesempenho2( Corpus &corpus, int atributo_padrao, int atributo_teste, int atributo_considerar ){return vector<float>();}
+
+};
+
+#endif // AVALIADOR_MATRIZ_CONFUSAO_H
