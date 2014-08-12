5f00476961aba5b6b5d92537cc91df0b80116e64
duarte.julio <duarte.julio@gmail.com>
2014-08-12 18:17:41 +0000

    atualização do kdobras para alterar atributo alvo na saida com respostas das dobras

--- validador/validadorkdobras.cpp
+++ validador/validadorkdobras.cpp
@@ -14,28 +14,45 @@ ValidadorKDobras::~ValidadorKDobras()
 vector< vector< float > > ValidadorKDobras::executarExperimento( Treinador &treinador, Corpus &corpus, int atributoTreino, int atributoTeste )
 {
     Classificador *classificador;
-    int i, j, qtd_sentencas = corpus.pegarQtdSentencas();
+    int i, j, qtd_ConjExemplos = corpus.pegarQtdSentencas(), qtdExemplos, c, e;
     vector< int> vetDobras;
     vector< vector< float > > resultado;
     vector< Corpus* > vetCorpus;
-    vector< bool > vetMascara(qtd_sentencas);
+    vector< bool > vetMascara(qtd_ConjExemplos);
 
-    for( i = 0; i < qtd_sentencas; ++i )
+    for( i = 0; i < qtd_ConjExemplos; ++i )
         vetDobras.push_back(i%numeroIteracoes);
 
     random_shuffle( vetDobras.begin(), vetDobras.end() );
 
     for( i = 0; i < numeroIteracoes; ++i )
     {
-        for( j = 0; j < qtd_sentencas; ++j )
+        //cria vetor de mascara
+        for( j = 0; j < qtd_ConjExemplos; ++j )
             vetMascara[j] = (vetDobras[j] == i);
 
+        //cria corpora em função da máscara
         vetCorpus = corpus.splitCorpus( vetMascara );
+
+        //treina o classificador
         classificador = treinador.executarTreinamento( *vetCorpus[0], atributoTreino );
 
+        //aplica classificador no corpus de teste
         classificador->executarClassificacao( *vetCorpus[1], atributoTeste );
 
+        //armazena resultado
         resultado.push_back( avaliador->calcularDesempenho( *vetCorpus[1], atributoTreino, atributoTeste ) );
+
+        //copia atributo teste para corpus original
+        j = 0;
+        for (c = 0; c < qtd_ConjExemplos; c++)
+            if (vetMascara[c]){
+                qtdExemplos = corpus.pegarQtdExemplos(c);
+                for (e=0; e<qtdExemplos; e++)
+                    corpus(c, e, atributoTeste, (*vetCorpus[1])(j, e, atributoTeste));
+                j++;
+            }
+
 
         delete vetCorpus[0];
         delete vetCorpus[1];
